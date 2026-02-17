6bddf779b40c4fbd52ff541ead97f24af2b90f73
Chirag Moolya <chiragmoolya13@gmail.com>
2026-02-17 08:32:07 +0530

    "made some changes"

--- Learn the basics/Things to know in Java/Array.java
+++ Learn the basics/Things to know in Java/Array.java
@@ -1,11 +1,10 @@
 public class Array {
     public static void main(String args[]){
         int arr[] = new int[5];
-
-        for(int i = 0; i<arr.length;i++){
+        for(int i = 0; i<5;i++){
             arr[i]=i;
         }
-        for(int i = 0; i<arr.length;i++){
+        for(int i = 0; i<5;i++){
             System.out.println(arr[i]);;
         }
     }
