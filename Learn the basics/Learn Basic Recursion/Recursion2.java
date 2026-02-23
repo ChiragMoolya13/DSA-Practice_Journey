c31b6c74c8fcb51159c0a00cc2b047042d2d846a
Chirag Moolya <chiragmoolya13@gmail.com>
2026-02-23 14:19:08 +0530

    findin sum and factorial of numbers with recursion

---
+++ Learn the basics/Learn Basic Recursion/Recursion2.java
@@ -0,0 +1,38 @@
+public class Recursion2 {
+    public static void SumOfN(int i,int n){
+        if(i<1){
+            System.out.println(n);
+            return;
+        }
+        SumOfN(i-1,n+i);
+    }
+    public static int optiSumOfN(int n){
+        if(n == 0){
+            return 0;
+        }
+        return n + optiSumOfN(n-1);
+    }
+    public static void FactofN(int i, int n){
+        if(i<1){
+            System.out.println(n);
+            return;
+        }
+        FactofN(i-1, n*i);
+    }
+    public static int optiFactOfN(int n){
+        if(n == 1){
+            return 1;
+        }
+        return n * optiFactOfN(n-1);
+    }
+    public static void main(String args[]){
+        // int n = optiSumOfN(4);
+        // System.out.println(n);
+        // int n = 3;
+        // SumOfN(n, 0 );
+        int n = optiFactOfN(5);
+        System.out.println(n);
+        // int n = 4;
+        // FactofN(n, 1);
+    }
+}
