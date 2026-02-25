23f9b8bd0aa5c81ae32d50f31b179ce204f56432
Chirag Moolya <chiragmoolya13@gmail.com>
2026-02-25 11:23:47 +0530

    Hash understanding

---
+++ Learn the basics/Learn Basic Hashing/Hash.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Hash {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = 5;
+        int arr[] = new int[n];
+        for(int i = 0; i < n;i++){
+            arr[i] = sc.nextInt();
+        }
+
+        int hash[] = new int[13];
+        for(int i = 0; i<n;i++){
+            hash[arr[i]] += 1; 
+        }
+
+        int q = sc.nextInt();
+        while(q>0){
+            System.out.println("Enter numbers: ");
+            int number = sc.nextInt();
+            System.out.println(hash[number]);
+            q--;
+        }
+    }
+}
