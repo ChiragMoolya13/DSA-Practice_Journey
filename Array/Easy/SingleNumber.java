3afc23ce03d24288ceb2509698b35e5b46536a1e
Chirag Moolya <chiragmoolya13@gmail.com>
2026-03-06 10:35:41 +0530

    Single Number

---
+++ Array/Easy/SingleNumber.java
@@ -0,0 +1,30 @@
+public class SingleNumber {
+    public static int singleNum(int[] arr){
+      
+        int ans = 0;
+        for (int i = 0; i < arr.length; i++) {
+            int num = arr[i];
+            int count = 0;
+            for (int j = 0; j < arr.length; j++) {
+                if(arr[j] == num){
+                    count++;
+                }
+            }
+            if(count == 1){
+                ans = num;
+            }
+        }
+        return ans;
+    }
+    public static int xor(int[] arr){
+        int xor = 0;
+        for (int i = 0; i < arr.length; i++) {
+            xor = xor ^ arr[i];
+        }
+        return xor;
+    }
+    public static void main(String args[]){
+        int[] arr = {1,1,2,2,3,4,4}; 
+        System.out.println(singleNum(arr));
+    }
+}
