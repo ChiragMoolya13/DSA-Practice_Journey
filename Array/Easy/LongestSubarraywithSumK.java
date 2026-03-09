c07617be8ec2de15f40926815714437107f5c23c
Chirag Moolya <chiragmoolya13@gmail.com>
2026-03-09 10:34:29 +0530

    Longest subarray with sum of K(brute force approch!)

---
+++ Array/Easy/LongestSubarraywithSumK.java
@@ -0,0 +1,21 @@
+public class LongestSubarraywithSumK {
+    public static int lSaWSK(int[] arr, int k){
+        int len = 0;
+        for(int i = 0; i<arr.length;i++){
+            int sum = 0;
+            for(int j = i; j<arr.length;j++){
+                sum = sum + arr[j];
+                if(sum == k){
+                    len = Math.max(len, j-i+1);
+                }
+            }
+        }
+        return len;
+    }
+    public static void main(String args[]){
+        int[] arr = {1,2,3,1,1,1,4,2,3};
+        int ans = lSaWSK(arr, 3);
+        System.out.print(ans);
+
+    }
+}
