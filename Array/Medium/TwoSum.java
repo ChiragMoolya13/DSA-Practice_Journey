01f5b8d5bdb5a7e3afb712fa0663c95412d68ec0
Chirag Moolya <chiragmoolya13@gmail.com>
2026-03-11 10:04:02 +0530

    Two Sum optimized approch using hashmap

--- Array/Medium/TwoSum.java
+++ Array/Medium/TwoSum.java
@@ -1,3 +1,6 @@
+
+import java.util.HashMap;
+
 public class TwoSum {
     // Brute Force Approch
     public static int[] twoSum(int[] arr, int target){
@@ -11,14 +14,26 @@ public class TwoSum {
         }
         return new int[]{-1,-1};
     }
-    // // Better Approch
-    // public static int[] twoSumBetter(int[] arr, int target){
-
-    // }
+    // Better Approch
+    public static int[] twoSumBetter(int[] arr, int target){
+        HashMap <Integer,Integer> map = new HashMap<>();
+        for(int i = 0;i<arr.length;i++){
+            int more = target - arr[i];
+            if(map.containsKey(more)){
+                return new int[] {map.get(more), i};
+            }
+            else{
+                map.put(arr[i], i);
+            }
+        }
+        return new int[] {-1,-1};
+    }
     public static void main(String args[]){
         int[] arr = {2,6,5,8,11};
         int target = 14;
         int[] res = twoSum(arr,target);
         System.out.println("[" + res[0] + ", " + res[1]+"]");
+        int[] res2 = twoSumBetter(arr, target);
+        System.out.println("[" + res2[0] + ", " + res2[1]+"]");
     }
 }
