6c7b8e0950cceafcd637188880627dfa33221073
Chirag Moolya <chiragmoolya13@gmail.com>
2026-03-17 10:00:03 +0530

    Moore's Voting Algorithm

--- Array/Medium/MajorityElements.java
+++ Array/Medium/MajorityElements.java
@@ -15,8 +15,36 @@ public class MajorityElements {
         }
         return -1;
     }
+    // Optimized approch using Moore's Voting Algorithm
+    public static int MooresVotingAlgo(int[] arr){
+        int count = 0;
+        int el = 0;
+        for(int i = 0;i<arr.length;i++){
+            if(count == 0){
+                count=1;
+                el = arr[i];
+            }
+            else if (arr[i] == el){
+                count++;
+            }
+            else{
+                count--;
+            }
+        }
+        int cnt1=0;
+        for (int i = 0; i < arr.length; i++) {
+             if(arr[i]==el){
+                cnt1++;
+             }
+        }
+        if(cnt1>arr.length/2){
+            return el;
+        }
+        return -1;
+    }
     public static void main(String args[]){
         int[] arr = {2,2,1,1,2,2};
         System.out.println(MajorityElem(arr));
+        System.out.println(MooresVotingAlgo(arr));
     }
 }
